8a1d178663851d10b06e55f9b651afd94bbfd72c
윤창현 <changh950@gmail.com>
2022-03-16 20:23:13 +0900

    ✨ javascript 기본 event 구현
    ✨ javascript if문 활용 색상변경
    
    ✨ javascript classList 기본 if문 사용
    
    ✨ javascript classList toggle을 활용한 간결한 코드 작성

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-alert("초기세팅");
+const title = document.querySelector(".hello h1");
+
+console.dir(title);
+
+function handleTitleClick() {
+  title.classList.toggle("active");
+}
+
+function handleMouseEnter() {
+  title.innerText = "마우스 여기있다";
+}
+
+function handleMouseLeave() {
+  title.innerText = "마우스 여기없다";
+}
+
+function handleWindowResize() {
+  document.body.style.backgroundColor = "tomato";
+}
+
+function handleWindowCopy() {
+  alert("copier!");
+}
+
+function handleWindowOffline() {
+  alert("SOS no WIFI");
+}
+
+function handleWindowOnline() {
+  alert("ALL GOOD");
+}
+
+title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
