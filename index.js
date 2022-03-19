972de578009132278c327a02bab154c2f57b52a7
윤창현 <changh950@gmail.com>
2022-03-19 15:35:23 +0900

    ✨ javascript submit 후 hidden class추가 및 변수활용

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const logInForm = document.querySelector(".login-form");
 const logInInput = document.querySelector(".login-form input");
+const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
+
+const HIDDEN_CLASS_NAME = "hidden";
 
 function onLogInSubmit(event) {
   const userName = logInInput.value;
   event.preventDefault();
-  console.log(userName);
+  logInForm.classList.add(HIDDEN_CLASS_NAME);
+  greeting.innerText = `Hello ${userName}`;
+  greeting.classList.remove(HIDDEN_CLASS_NAME);
 }
 
 function handleLinkClick(event) {
