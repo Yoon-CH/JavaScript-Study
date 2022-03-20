4d44f3f92e5cb58419888a0525f6f21b5e15f617
윤창현 <changh950@gmail.com>
2022-03-20 13:10:04 +0900

    ✨ javascript localStorage 활용

--- index.js
+++ index.js
@@ -4,19 +4,26 @@ const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
 const HIDDEN_CLASS_NAME = "hidden";
+const USERNAME_KEY = "userName";
+
+const savedUserName = localStorage.getItem(USERNAME_KEY);
 
 function onLogInSubmit(event) {
   const userName = logInInput.value;
   event.preventDefault();
   logInForm.classList.add(HIDDEN_CLASS_NAME);
-  greeting.innerText = `Hello ${userName}`;
+  localStorage.setItem(USERNAME_KEY, userName);
+  paintGreetings(userName);
+}
+
+function paintGreetings(userInfo) {
+  greeting.innerText = `Hello ${userInfo}`;
   greeting.classList.remove(HIDDEN_CLASS_NAME);
 }
 
-function handleLinkClick(event) {
-  event.preventDefault();
-  console.dir(event);
+if (savedUserName === null) {
+  logInForm.addEventListener("submit", onLogInSubmit);
+} else {
+  logInForm.classList.add(HIDDEN_CLASS_NAME);
+  paintGreetings(savedUserName);
 }
-
-logInForm.addEventListener("submit", onLogInSubmit);
-link.addEventListener("click", handleLinkClick);
