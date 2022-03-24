1998b98ec51d94fb4d2d8bc52854c78b355e6dca
윤창현 <changh950@gmail.com>
2022-03-24 11:48:49 +0900

    ✨ javascript todo리스트 삭제하기 구현

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
 
+function deleteToDo(event) {
+  const deleteLi = event.target.parentElement;
+  deleteLi.remove();
+}
+
 function paintToDo(newTodo) {
   const li = document.createElement("li");
   const span = document.createElement("span");
-  li.appendChild(span);
   span.innerText = newTodo;
+  const button = document.createElement("button");
+  button.innerText = "✖︎";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
   todoList.appendChild(li);
 }
 
